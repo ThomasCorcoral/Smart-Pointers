2c133e0300739fa3808c35bb29ef057e08b099e2
ThomasCorcoral <thomas.corcoral@edu.univ-fcomte.fr>
2021-02-01 17:00:34 +0100

    unique ptr

--- Unique.h
+++ Unique.h
@@ -5,41 +5,56 @@ namespace sp {
   template<typename T>
   class Unique {
   public:
-    Unique(T* ptr = nullptr) {
+    Unique(T* ptr = nullptr){
     }
 
+    // Suppression
     ~Unique() {
+      if(my_ptr){
+        delete my_ptr
+      }
     }
 
+    // Constructeur par copie (Donc interdit)
     Unique(const Unique<T>& other) = delete;
 
-    Unique(Unique&& other) {
+    // Constructeur par déplacement
+    Unique(Unique&& other):m_unique_ptr(std::exchange(other.m_unique_ptr, nullptr)){ // && objet temporaire
     }
 
+    // Suppression opérateur =
     Unique& operator=(const Unique& other) = delete;
 
-    Unique& operator=(Unique&& other) {
+    // Surchage opérateur = copie par déplacement
+    Unique& operator=(Unique&& other):m_unique_ptr(delete){
+      /*if(m_unique_ptr){
+        delete m_unique_ptr;
+      }*/
+      m_unique_ptr = std::exchange(other.m_unique_ptr, nullptr);
       return *this;
     }
 
     T* get() {
-      return nullptr;
+      return m_unique_ptr;
     }
 
+    // Surcharge opérateur *
     T& operator*() {
       return T();
     }
 
+    // Surcharge opérateur ->
     T* operator->() {
       return nullptr;
     }
 
     bool exists() const {
-      return false;
+      return m_unique_ptr != nullptr
     }
 
   private:
     // implementation defined
+    T *m_unique_ptr;
   };
 }
 
